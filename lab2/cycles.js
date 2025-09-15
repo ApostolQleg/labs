4a9279bc65ac54be482a9ba816ce9eaa21e54b50
ApostolOleg <apostololeg0@gmail.com>
2025-09-15 18:50:13 +0300

    lab2 modified

--- lab2/cycles.js
+++ lab2/cycles.js
@@ -1,11 +1,15 @@
+// Cycles
+
+// Function that creates an array of all the numbers from some number to another
 let range = (start, end) => {
     let array = [];
     for (let i = start; i <= end; i++) array.push(i);
     return array;
-};
+}; 
 
 console.dir(range(15, 30));
 
+// Function that creates an array of odd numbers from some number to another
 let rangeOdd = (start, end) => {
     let array = [];
     for (let i = start; i <= end; i++) if (i % 2) array.push(i);
--- lab2/identificators.js
+++ lab2/identificators.js
@@ -1,7 +1,12 @@
+// Identificators
+
+// Common variable
 let myName = "Oleg";
 
+// Constant variable
 const myBirthDate = 2008;
 
+// Function that greets with the single argument
 const hello = (name) => {
     console.log(`Hello, ${name}!`);
 };

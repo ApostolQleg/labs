0a550ac7d9ecae6221f2b792a13d2c7f95d730be
ApostolOleg <apostololeg0@gmail.com>
2025-09-23 19:37:13 +0300

    create lab4

---
+++ lab3/methods.js
@@ -0,0 +1,21 @@
+const iface = {
+    m1: x => [x],
+    m2: function (x, y) {
+        return [x, y];
+    },
+    m3(x, y, z) {
+        return [x, y, z];
+    }
+};
+
+const methods = (obj) => {
+    const arr = [];
+    for (let key in obj) {
+        if (typeof obj[key] === 'function') {
+            arr.push([key, obj[key].length]);
+        }
+    }
+    return arr;
+};
+
+console.table(methods(iface));

0572244f12a66a988d2525d72d437bf2fabf0615
ApostolOleg <apostololeg0@gmail.com>
2025-09-26 18:43:23 +0300

    create lab6

---
+++ lab6/compose.js
@@ -0,0 +1,28 @@
+// Умова Шемсадінова
+const inc = (x) => ++x;
+const twice = (x) => x * 2;
+const cube = (x) => x ** 3;
+
+const f1 = pipe(inc, twice, cube); // Выводит: 1728
+console.log(f1(5));
+
+const f2 = pipe(inc, inc); // Выводит: 9
+console.log(f2(7));
+
+const f3 = pipe(inc, 7, cube); // Выводит ошибку
+console.log(f3(10));
+
+// виконання
+function pipe(...functions) {
+	let calc = (x) => {
+		try {
+			for (let f of functions.reverse()) {
+				x = f(x);
+			}
+			return x;
+		} catch (error) {
+			return "undefined";
+		}
+	};
+	return calc;
+}

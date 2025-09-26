43bc3017cb8629b71fa06a6ec3486dcc32e20777
ApostolOleg <apostololeg0@gmail.com>
2025-09-26 21:52:31 +0300

    create lab7

---
+++ lab7/1-remove.js
@@ -0,0 +1,28 @@
+// Умова Шемседінова
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
+console.log(array);
+// Результат: [1, 2, 3, 4, 6, 7]
+
+// виконання
+function removeElement(arr, item) {
+	for (let element of arr) {
+		if (element === item) {
+			arr.splice(arr.indexOf(element), 1);
+		}
+	}
+	return arr;
+}
+
+// Умова Шемсадінова
+const cities = ["Kiev", "Beijing", "Lima", "Washington"];
+removeElements(cities, "Lima", "Berlin", "Kiev");
+console.log(cities);
+// Результат: ['Beijing', 'Washington']
+
+// виконання
+function removeElements(arr, ...items) {
+	for (let item of items) {
+		removeElement(arr, item);
+	}
+}
